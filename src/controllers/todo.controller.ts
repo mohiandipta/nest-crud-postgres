105e44525a737ff5649efd0904af4e5732da0a38
mohiandipta <mohianmostofa@gmail.com>
2022-12-24 13:08:39 +0600

    controller created

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,16 +1,44 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Get, Req, Put, Delete, Param } from "@nestjs/common";
+import { Request } from "express";
+import { TodoInterface, TodosService } from "src/services/todo.services";
 
 interface CreateTodoDTO{
     name: string,
     complete: boolean
 }
 
-@Controller('Cats')
+@Controller('cats')
 export class TodoController {
-    // constructor(private todosService: TodosService){}
+    constructor(private todosService: TodosService){}
 
+    // GET
+    @Get()
+    async findAll(@Req() request:Request){
+        const cats: Array<TodoInterface> = await this.todosService.findAll()
+        return cats
+    }
+
+    // POST
     @Post()
-    async create(@Body() createTodoDto: CreateTodoDTO) {
-        // const todo = await this.todosService.
+    async create(@Body() createTodoDto: CreateTodoDTO){
+        const todo = await this.todosService.create(createTodoDto)
+        if (!todo) {
+            return 'error in creating todo'
+        }
+        return 'todo created successfully'
+    }
+
+    // PUT
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() body: any) {
+        const newCat: any = await this.todosService.update(id, body)
+        return "cat updated";
+    }
+
+    // DELETE
+    @Delete(':id')
+    async remove(@Param('id') id: string) {
+        await this.todosService.delete(id)
+        return "cat deleted"
     }
 }
